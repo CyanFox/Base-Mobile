f0ed91204df41b906967e08a4c8705532cde1731
Lenny P. | RealZone22 <lenny@petschl.org>
2025-01-13 21:52:38 +0100

    Add a check to check if an abort call was made. If yes than there will be no console logging

--- app/Traits/WithCustomLivewireException.php
+++ app/Traits/WithCustomLivewireException.php
@@ -5,11 +5,16 @@ namespace App\Traits;
 use Exception;
 use Filament\Notifications\Notification;
 use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
 trait WithCustomLivewireException
 {
     public function exception($e, $stopPropagation)
     {
+        if ($e instanceof HttpException) {
+            return;
+        }
+
         if ($e instanceof Exception) {
             $this->log($e->getMessage(), 'error');
         } else {
