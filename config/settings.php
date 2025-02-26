76ebb2e26e4881c061d9d4a25c1007672bb84f9a
RealZone22 | Lenny P. <lenny@petschl.org>
2025-02-26 19:30:23 +0100

    Add #466 Fix #479

--- config/settings.php
+++ config/settings.php
@@ -3,4 +3,6 @@
 return [
     'base_url' => env('BASE_VERSION_URL', 'https://raw.githubusercontent.com/CyanFox-Projects/CyanFox-Base/v4/version.json'),
     'logo_path' => '/img/Logo.svg',
+    'disable_db_settings' => env('DISABLE_DB_SETTINGS', false),
+    'settings.force_https' => env('FORCE_HTTPS', false),
 ];
