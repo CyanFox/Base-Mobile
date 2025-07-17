541c35c1f0ba89e1de500536f04703b46fe543c3
Lenny Petschl <lenny@petschl.org>
2025-07-17 10:57:24 +0000

    Ignore SpotlightSearchable as its only used in modules

--- app/Traits/WithCustomLivewireException.php
+++ app/Traits/WithCustomLivewireException.php
@@ -4,7 +4,6 @@ namespace App\Traits;
 
 use Exception;
 use Filament\Notifications\Notification;
-use Illuminate\Support\Facades\Log;
 use Illuminate\Validation\ValidationException;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 
@@ -22,7 +21,7 @@ trait WithCustomLivewireException
             $this->log($e, 'error'); // @phpstan-ignore-line
         }
 
-        if (! $e instanceof ValidationException) {
+        if (!$e instanceof ValidationException) {
             Notification::make()
                 ->title(__('messages.notifications.something_went_wrong'))
                 ->danger()
