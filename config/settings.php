37ba108c96efb13da7129582ff6e7806ae4c8e67
Lenny Petschl <lenny@petschl.org>
2025-07-25 17:46:32 +0000

    Rename repository from cyanfox-base to base

--- config/settings.php
+++ config/settings.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'base_url' => env('BASE_VERSION_URL', 'https://raw.githubusercontent.com/CyanFox-Projects/CyanFox-Base/v4/version.json'),
+    'base_url' => env('BASE_VERSION_URL', 'https://raw.githubusercontent.com/CyanFox/Base/v4/version.json'),
     'logo_path' => '/img/Logo.svg',
     'disable_db_settings' => env('DISABLE_DB_SETTINGS', false),
     'settings.force_https' => env('FORCE_HTTPS', false),
